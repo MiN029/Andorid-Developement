af17503181096085d29f82f98ca8a1f34abda324
SBILHBANK\chanmony.sovan <chanmony.sovan@sbilhbank.com.kh>
2023-04-10 16:48:24 +0700

    Complete with ViewModel

---
+++ EasyFoodMVVM/app/src/main/java/com/example/easyfoodmvvm/activity/MealActivity.kt
@@ -0,0 +1,12 @@
+package com.example.easyfoodmvvm.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.easyfoodmvvm.R
+
+class MealActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meal)
+    }
+}
--- EasyFoodMVVM/app/src/main/java/com/example/easyfoodmvvm/fragments/HomeFragment.kt
+++ EasyFoodMVVM/app/src/main/java/com/example/easyfoodmvvm/fragments/HomeFragment.kt
@@ -1,22 +1,27 @@
 package com.example.easyfoodmvvm
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.example.easyfoodmvvm.databinding.FragmentHomeBinding
 import com.example.easyfoodmvvm.pojo.Meal
-import com.example.easyfoodmvvm.pojo.MealList
-import com.example.easyfoodmvvm.retrofit.RetrofitInstance
-import retrofit2.Call
-import retrofit2.Response
+import com.example.easyfoodmvvm.viewModel.HomeViewModel
 
 class HomeFragment : Fragment() {
 
-    private lateinit var binding : FragmentHomeBinding
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var mHomeViewModel: HomeViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mHomeViewModel = ViewModelProvider(this@HomeFragment).get(HomeViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,24 +34,25 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mHomeViewModel.getRandomMeal()
+        observeRandomMealLiveData()
+        onRandomMealClikc()
+    }
 
-        RetrofitInstance.api.getRandomMeal().enqueue(object : retrofit2.Callback<MealList>{
-            override fun onResponse(call: Call<MealList>, response: Response<MealList>) {
-                if (response.body() != null){
-                    val randomMeal : Meal = response.body()!!.meals[0]
-                    Glide.with(this@HomeFragment)
-                        .load(randomMeal.strMealThumb)
-                        .into(binding.imgRadomMeal)
-                }else{
-                    return
-                }
-            }
+    private fun onRandomMealClikc() {
+        binding.imgRadomMeal.setOnClickListener {
+            val intent : ()
+        }
+    }
 
-            override fun onFailure(call: Call<MealList>, t: Throwable) {
-                Log.d("HomeFragment" , t.message.toString())
+    private fun observeRandomMealLiveData() {
+        mHomeViewModel.observeRandomMealLiveData().observe(viewLifecycleOwner , object : Observer<Meal>{
+            override fun onChanged(value: Meal) {
+                Glide.with(this@HomeFragment)
+                    .load(value.strMealThumb)
+                    .into(binding.imgRadomMeal)
             }
 
         })
-
     }
 }
